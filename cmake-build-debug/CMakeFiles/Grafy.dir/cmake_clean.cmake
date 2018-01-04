a3c9d046fc814c821c99b9086de721b08d84da90
B.Cybulski <B.Cybulski@stud.elka.pw.edu.pl>
2018-01-05 00:23:57 +0100

    Hello

---
+++ cmake-build-debug/CMakeFiles/Grafy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Grafy.dir/main.cpp.o"
+  "Grafy.pdb"
+  "Grafy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Grafy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Graphs!" << std::endl;
+    return 0;
+}
